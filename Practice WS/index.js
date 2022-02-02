984d1064b93dcd5545aacee622567609f022b1fd
sudhirbarik2 <sudhir.barik981@gmail.com>
2022-02-02 22:03:27 +0530

    codes

--- Practice WS/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var cors = require('cors')
-const app=express();
-const data=require('./data.json')
-app.use(cors())
-app.get('/', function (req, res) {
-    res.send(data)
-  })
-app.listen(4000,()=>{console.log("Running on 4000");})
